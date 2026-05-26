74c45a05c467a578d3f41019f452074168ee5179
Gregor Sönnichsen <gregor.soennichsen@outlook.com>
2026-05-26 18:29:23 +0200

    Implemented LineOfSight conditional (untested)

---
+++ Source/SFConditional/Private/ConditionalTypes/Spatial/Conditional_Spatial_LineOfSight.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright Strayfarer & Contributors. Released under the MIT license.
+
+
+#include "ConditionalTypes/Spatial/Conditional_Spatial_LineOfSight.h"
+
+#include "Kismet/GameplayStatics.h"
+
+SF::FConditionalAnswer SF::UConditional_Spatial_LineOfSight::EvaluateInternal_Implementation(
+	const FConditionalEvaluationContext& EvaluationContext)
+{
+	using namespace SF::Conditional;
+	
+	const TOptional<FTransform> InstigatorWorldTransform = EvaluationContext.TryGetInstigatorTransform();
+	if (!InstigatorWorldTransform.IsSet())
+	{
+		return Answer::Error::Instigator::NoTransformProvider(EvaluationContext.GetInstigator());
+	}
+	
+	const APlayerController* Pc = UGameplayStatics::GetPlayerController(EvaluationContext.GetWorld(), 0);
+	if (!Pc || !Pc->GetLocalPlayer())
+	{
+		return Answer::Error::NoPlayerController(EvaluationContext.GetWorld());
+	}
+
+	const FVector CameraForward = Pc->PlayerCameraManager->GetActorForwardVector();
+	const FVector TraceStart = InstigatorWorldTransform.GetValue().GetLocation();
+	const FVector TraceEnd = TraceStart + MaxDistance * CameraForward;
+	
+	FCollisionQueryParams TraceParams;
+	TraceParams.TraceTag = GetFName();
+	if (bShouldIgnoreInstigator)
+	{
+		TraceParams.AddIgnoredSourceObject(EvaluationContext.GetInstigator());
+	}
+		
+	FHitResult HitResult;
+	const bool bDidHit = EvaluationContext.GetWorld()
+		->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, CollisionChannelToCheck, TraceParams);
+	return Answer::FromBool(bDidHit);
+}
+
+FString SF::UConditional_Spatial_LineOfSight::CreateConfigurationDebugString_Implementation() const
+{
+	return FString::Printf(TEXT("MaxDistance %f"), MaxDistance);
+}
+
+#if WITH_GAMEPLAY_DEBUGGER
+void SF::UConditional_Spatial_LineOfSight::VisualizeWithGameplayDebugger(FGameplayDebuggerCategory& Debugger,
+	FGameplayDebuggerCanvasContext& Canvas)
+{
+	
+}
+#endif // WITH_GAMEPLAY_DEBUGGER
---
+++ Source/SFConditional/Public/ConditionalTypes/Spatial/Conditional_Spatial_LineOfSight.h
@@ -0,0 +1,44 @@
+﻿// Copyright Strayfarer & Contributors. Released under the MIT license.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Conditional.h"
+
+#include "Conditional_Spatial_LineOfSight.generated.h"
+
+namespace SF
+{
+	/**
+	* Returns whether the tested location is hit by line traces starting from the instigator.
+	* 
+	* Notes:
+	* - FuzzyScore returns 0.f if outside the radius, else closeness [0.f - 1.f] to instigator.
+	* - Supports Actors, SceneComponents, ActorComponents (using owner transform).
+	*/
+	UCLASS(DisplayName="SPATIAL - Line of Sight")
+	class SFCONDITIONAL_API UConditional_Spatial_LineOfSight : public UConditional
+	{
+		GENERATED_BODY()
+
+	protected:
+		// UConditional
+		virtual FConditionalAnswer EvaluateInternal_Implementation(
+			const FConditionalEvaluationContext& EvaluationContext) override;
+		virtual FString CreateConfigurationDebugString_Implementation() const override;
+#if WITH_GAMEPLAY_DEBUGGER
+		virtual void VisualizeWithGameplayDebugger(FGameplayDebuggerCategory& Debugger,
+												   FGameplayDebuggerCanvasContext& Canvas) override;
+#endif // WITH_GAMEPLAY_DEBUGGER
+		// --
+		
+		UPROPERTY(EditAnywhere)
+		float MaxDistance = 200.f;
+		
+		UPROPERTY(EditAnywhere)
+		ECollisionChannel CollisionChannelToCheck = ECC_Visibility;
+		
+		UPROPERTY(EditAnywhere)
+		bool bShouldIgnoreInstigator = true;
+	};
+}
