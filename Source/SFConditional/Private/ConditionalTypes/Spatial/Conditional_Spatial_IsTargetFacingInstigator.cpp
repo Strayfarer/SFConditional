d7af5d7d202daed9eba8d150ce4013f81cd0f204
Gregor Sönnichsen <gregor.soennichsen@outlook.com>
2026-06-01 18:59:00 +0200

    Added test object debug visualization for IsTargetFacingInstigator conditional

--- Source/SFConditional/Private/ConditionalTypes/Spatial/Conditional_Spatial_IsTargetFacingInstigator.cpp
+++ Source/SFConditional/Private/ConditionalTypes/Spatial/Conditional_Spatial_IsTargetFacingInstigator.cpp
@@ -40,3 +40,37 @@ FString SF::UConditional_Spatial_IsTargetFacingInstigator::CreateConfigurationDe
 {
 	return FString::Printf(TEXT("MaxAngle: %.0f°"), MaxAngle);
 }
+
+#if WITH_GAMEPLAY_DEBUGGER
+void SF::UConditional_Spatial_IsTargetFacingInstigator::VisualizeTestObjectWithGameplayDebugger(
+	const FConditionalEvaluationContext& EvaluationContext, FGameplayDebuggerCategory& Debugger,
+	FGameplayDebuggerCanvasContext& Canvas)
+{
+	Super::VisualizeTestObjectWithGameplayDebugger(EvaluationContext, Debugger, Canvas);
+
+	const TOptional<FTransform> TestTransform = EvaluationContext.TryGetTestObjectTransform();
+	if (!TestTransform.IsSet())
+		return;
+	
+	const TOptional<FTransform> InstigatorTransform = EvaluationContext.TryGetInstigatorTransform();
+	if (!InstigatorTransform.IsSet())
+		return;
+	
+	const FVector TargetToInstigator = InstigatorTransform.GetValue().GetLocation() - TestTransform.GetValue().GetLocation();
+	const FVector TargetForward = InstigatorTransform.GetValue().Rotator().Vector();
+	const float Angle = TargetForward.CosineAngle2D(TargetToInstigator);
+	const float NormalizedAngle = (Angle + 1.f) / 2.f;
+	const float DegreesAngle = NormalizedAngle * 180.f;
+	const bool bBinaryAnswer = DegreesAngle <= MaxAngle / 2.f;
+	
+	const FVector Left = TargetForward.GetSafeNormal2D().RotateAngleAxis(MaxAngle, FVector::UpVector);
+	const FVector Right = TargetForward.GetSafeNormal2D().RotateAngleAxis(-MaxAngle, FVector::UpVector);
+	
+	const FColor Color = bBinaryAnswer ? FColor::Green : FColor::Cyan;
+	DrawDebugCircleArc(EvaluationContext.GetWorld(), TestTransform.GetValue().GetLocation(), 200.f, -TargetForward.GetSafeNormal2D(), FMath::DegreesToRadians(MaxAngle), 32, Color);
+	DrawDebugLine(EvaluationContext.GetWorld(), TestTransform.GetValue().GetLocation(), TestTransform.GetValue().GetLocation() - TargetForward.GetSafeNormal2D() * 200.f, Color);
+	DrawDebugLine(EvaluationContext.GetWorld(), TestTransform.GetValue().GetLocation(), TestTransform.GetValue().GetLocation() - Left.GetSafeNormal2D() * 200.f, Color);
+	DrawDebugLine(EvaluationContext.GetWorld(), TestTransform.GetValue().GetLocation(), TestTransform.GetValue().GetLocation() - Right.GetSafeNormal2D() * 200.f, Color);
+	DrawDebugLine(EvaluationContext.GetWorld(), TestTransform.GetValue().GetLocation(), TestTransform.GetValue().GetLocation() + TargetToInstigator.GetSafeNormal2D()  * 200.f, FColor::Black, false, -1, 0, 2.f);
+}
+#endif
